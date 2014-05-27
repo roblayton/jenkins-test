03889aa4de0a54f9be6e94f9c75141ac768987c5
roblayton <robert.b.layton@gmail.com>
2014-05-27 03:18:11 -0400

    Initial commit

---
+++ spec/utils/Calc.spec.js
@@ -0,0 +1,7 @@
+describe('Calc', function() {
+    describe('#sum', function() {
+        it('should return the sum of two numbers', function() {
+            expect(Calc.sum(1, 2)).toEqual(3); 
+        });
+    });
+});
---
+++ src/utils/Calc.js
@@ -0,0 +1,5 @@
+var Calc = {};
+
+Calc.sum = function(a, b) {
+    return a + b;
+};
